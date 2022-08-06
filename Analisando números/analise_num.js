c361b10f5415730fc76b992cf7edde7a3d8362b5
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-08-06 17:06:18 -0300

    updadte

--- Analisando números/analise_num.js
+++ Analisando números/analise_num.js
@@ -24,10 +24,55 @@ function n_esta_list(n, l){
 
 
 function adn(){
-    //verificar se é um numero, e se não esta na lista
-    if (é_num(num.value) && n_esta_list(num.value, vetor)){
-        window.alert('Tudo ok')
+    //verificar se é um numero, e se não esta na lista (ponto de explacamação)
+    if (é_num(num.value) && ! n_esta_list(num.value, vetor)){
+        //adicionando valor no vetor
+        vetor.push(Number(num.value))
+        resp.innerHTML = 'Você clicou para adicionar mais números'
+
+        //criando uma tag
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        selec.appendChild(item)
     } else {
         window.alert('Valor invalido ou já encontrado na lista')
+    }
+    
+    //apagar a caixa e focar o mouse no input num
+    num.value = ' '
+    num.focus()
+}
+
+function finalizar(){
+    if (vetor.length == 0){
+        window.alert('Adicione pelemenos um valor antes de finalizar')
+    } else{
+        let total = vetor.length
+        let maior = vetor[0]
+        let menor = vetor[0]
+        let soma = 0
+        let media = 0
+
+        for (let c in vetor){
+            soma += vetor[c]
+
+            if (vetor[c] > maior)
+                maior = vetor[c]
+
+            if (vetor[c] < menor)
+                menor = vetor[c]
+        }
+        media = soma / total
+
+        resp.innerHTML = ' '
+        resp.innerHTML += `<p>Foram adicionado ${total} números no cadastro</p>`
+
+        resp.innerHTML += `<p>O <strong>maior</strong> valor adicionado foi ${maior}</p>`
+
+        resp.innerHTML += `<p>O <strong>menor</strong> valor adicionado foi ${menor}</p>`
+
+        resp.innerHTML += `<p>A <strong>soma</strong> dos valores adicionado foi ${soma}</p>`
+
+        resp.innerHTML += `<p>A <strong>media</strong> dos valores adicionado foi ${media}</p>`
     }
 }
