ae359d14a7664a9cd2a08e85466210860a9298b0
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-08-06 22:03:26 -0300

    update

--- lampada/edição2.js
+++ lampada/edição2.js
@@ -2,19 +2,23 @@ let lampada = window.document.querySelector('img#id_lampada')
 let ligar = window.document.querySelector('input#id_ligar')
 let desligar = window.document.querySelector('input#id_desligar')
 
+ligar = addEventListener('click', _ligar)
+desligar = addEventListener('click', _desligar)
+lampada = addEventListener('onmouseenter', dentro)
+lampada = addEventListener('onmouseout', fora)
 
-function btn_lig(){
+function _ligar(){
     lampada.src = 'imagens/ligada.jpg'
 }
 
-function btn_desl(){
+function _desligar(){
     lampada.src = 'imagens/desligada.jpg'
 }
 
-function dentro(){
+/*function dentro(){
     lampada.src = 'imagens/ligada.jpg'
 }
 
 function fora(){
     lampada.src = 'imagens/desligada.jpg'
-}
+}*/
