6bc4d571a59d832c5281e568812972d51eaf3558
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-08-07 17:05:05 -0300

    update

---
+++ Modal/edição.js
@@ -0,0 +1,4 @@
+let abrir = window.document.querySelector('button#abrir-modal')
+let fechar = window.document.querySelector('button#fechar-modal')
+let modal = window.document.querySelector('div#box-modal')
+let fade = window.document.querySelector('div#id_fade')
