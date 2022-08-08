c06aed99cfdccffed42cf225fed5959d514567b9
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-08-08 12:07:14 -0300

    update

--- Calculadora/edição.js
+++ Calculadora/edição.js
@@ -2,3 +2,11 @@ let op_ant = window.document.querySelector('div#operação_anterior')
 let op_digi = window.document.querySelector('div#oparação_digitada')
 
 let botoes = window.document.querySelectorAll('div#oparação_digitada button')
+
+
+botoes.forEach((btns) => {
+    btns.addEventListener("click", (evento) => {
+        let valor = evento.target.innerText;
+        console.log(valor)
+    })
+})
