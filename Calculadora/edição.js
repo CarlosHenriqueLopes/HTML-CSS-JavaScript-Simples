a4eae958fa4fc3a5e5120130c111d1acda75c9fe
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-08-08 11:40:23 -0300

    update

---
+++ Calculadora/edição.js
@@ -0,0 +1,4 @@
+let op_ant = window.document.querySelector('div#operação_anterior')
+let op_digi = window.document.querySelector('div#oparação_digitada')
+
+let botoes = window.document.querySelectorAll('div#oparação_digitada button')
