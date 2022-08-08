b9b1acefc4e94109e8ca709c387a0bcc637c370d
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-08-08 16:15:41 -0300

    update

--- Calculadora/edição.js
+++ Calculadora/edição.js
@@ -4,9 +4,33 @@ let op_digi = window.document.querySelector('div#oparação_digitada')
 let botoes = window.document.querySelectorAll('div#oparação_digitada button')
 
 
+class calculadora {
+    constructor(op_anterior, op_digitada){
+        this.op_ant = op_anterior
+        this.op_digi = op_digitada
+        this.digito_agr = ''
+
+    }
+
+    // adidionar os digitos na tela
+    add_digito(digito){
+        console.log(digito)
+    }
+}
+
+let calcular = new calculadora(a, b)
+
+// para deixar os clicks dos botoes imprimirem textos
 botoes.forEach((btns) => {
     btns.addEventListener("click", (evento) => {
-        let valor = evento.target.innerText;
+        let valor = evento.target.innerText
         console.log(valor)
+
+        if (Number(valor.value) >=0 || valor.value === '.'){
+            //tratados como numeros
+            calculadora.add_digito(valor)
+        } else {
+
+        }
     })
 })
