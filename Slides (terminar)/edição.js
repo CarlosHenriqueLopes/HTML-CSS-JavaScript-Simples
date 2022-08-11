972bb90f9d1d185a5fc81a57a8c6206881bb93d6
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-08-11 00:22:55 -0300

    update

--- Slides (terminar)/edição.js
+++ Slides (terminar)/edição.js
@@ -29,10 +29,17 @@ loadImagem(imagens, box_imgs)
 let btn_avançar = window.document.querySelector('div#btn_avançar')
 let btn_voltar = window.document.querySelector('div#btn_voltar')
 
+let img1 = window.document.querySelector('img#img1')
+let img2 = window.document.querySelector('img#img2')
+let img3 = window.document.querySelector('img#img3')
+let img4 = window.document.querySelector('img#img4')
+let img5 = window.document.querySelector('img#img5')
+let img6 = window.document.querySelector('img#img6')
+
 function avançar(){
-    window.alert('teste avançar')
+    console.log('img1.innerHTML')
 }
 
-function volta(){
+function voltar(){
     window.alert('teste voltar')
 }
