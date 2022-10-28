0149cc3a483f056075357681cdd9dc28810b4a9d
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-10-28 17:13:28 -0300

    update

--- Modal (terminar)/edição.js
+++ Modal (terminar)/edição.js
@@ -2,3 +2,15 @@ let abrir = window.document.querySelector('button#abrir-modal')
 let fechar = window.document.querySelector('button#fechar-modal')
 let modal = window.document.querySelector('div#box-modal')
 let fade = window.document.querySelector('div#id_fade')
+
+/*
+// apagar depois (teste)
+let abrir = window.document.getElementById('modal-body')
+teste.addEventListener('click', function(){
+    if(teste.style.display === 'none'){
+        teste.style.display = 'block'
+    } else {
+        teste.style.display = 'none'
+    }
+})
+*/
