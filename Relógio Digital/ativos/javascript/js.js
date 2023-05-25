c99450a33e1a3e7e3fcdfeb67ddaec248187a46e
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2023-05-25 15:24:11 -0300

    up

--- Relógio Digital/ativos/javascript/js.js
+++ Relógio Digital/ativos/javascript/js.js
@@ -2,15 +2,25 @@ const horas = window.document.getElementById("horas");
 const min = window.document.getElementById("min");
 const seg = window.document.getElementById("seg");
 
+const btn = window.document.getElementsByClassName("relogio");
+const conteiner = window.document.getElementsByClassName("relogio");
+
+
 const relogio = setInterval(function time(){
     let DataHoje = new Date();
     let hrs = DataHoje.getHours();
     let minutos = DataHoje.getMinutes();
     let segundos = DataHoje.getSeconds();
 
-    if (hrs > 10) hrs = "" + hrs; 
-    if (minutos > 10) minutos = "" + minutos;
-    if (segundos > 10) segundos = "" + segundos;
+    // condições para acrescentar 0 antes de unidades
+    if (hrs < 10)
+        hrs = "0" + hrs;
+         
+    if (minutos < 10) 
+        minutos = "0" + minutos;
+
+    if (segundos < 10) 
+        segundos = "0" + segundos;
 
     horas.textContent = hrs;
     min.textContent = minutos;
