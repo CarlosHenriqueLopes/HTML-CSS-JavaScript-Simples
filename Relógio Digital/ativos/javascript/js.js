95edc9de54cdd5d9e422e78418ea398c0b1b84e3
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2023-05-28 23:15:15 -0300

    finalizado

--- Relógio Digital/ativos/javascript/js.js
+++ Relógio Digital/ativos/javascript/js.js
@@ -2,15 +2,19 @@ const horas = window.document.getElementById("horas");
 const min = window.document.getElementById("min");
 const seg = window.document.getElementById("seg");
 
-let btn = window.document.getElementsByClassName("btn");
-const conteiner1 = window.document.getElementsByClassName("conteiner1");
-let conteiner2 = window.document.getElementsByClassName("conteiner2");
+const btn = window.document.querySelector("button.btn");
+const conteiner1 = window.document.querySelector("div.conteiner1");
+const relogio_conteiner = window.document.querySelector("div.relogio");
 
+
+//função para quando clicar no btn fazer a troca da msg pelo mostrador de horas, e vice versa
 btn.addEventListener("click", function(){
-    if (conteiner2.style.display === "none"){
-        conteiner2.style.display = "block"
+    if (relogio_conteiner.style.display === "none"){
+        relogio_conteiner.style.display = "flex"
+        conteiner1.style.display = "none"
     } else {
-        conteiner2.style.display = "none"
+        conteiner1.style.display = "block"
+        relogio_conteiner.style.display = "none"
     }
 })
 
